857ebc50754054340967588475869101f1d3afb7
Artem Bolotov <teeemon@gmail.com>
2023-02-06 17:48:40 +0300

    Initial Commit

---
+++ FluxExample/FluxExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxExampleApp.swift
+//  FluxExample
+//
+//  Created by artembolotov on 06.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FluxExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
